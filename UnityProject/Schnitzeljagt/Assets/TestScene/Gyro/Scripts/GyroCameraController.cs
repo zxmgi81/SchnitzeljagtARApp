7b46ce16b0ac4b654235236ea3e4a1e6b0d6ce47
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-11-18 18:47:51 +0100

    technology test

---
+++ UnityProject/Schnitzeljagt/Assets/TestScene/Gyro/Scripts/GyroCameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCameraController : MonoBehaviour {
+
+    public float RotationMaxSpeed;
+
+	void Update () {
+        GyroModifyCamera();
+
+        transform.Rotate(Vector3.forward, Input.GetAxis("Horizontal") * RotationMaxSpeed);
+
+
+	}
+
+
+    // The Gyroscope is right-handed.  Unity is left handed.
+    // Make the necessary change to the camera.
+    void GyroModifyCamera()
+    {
+        transform.rotation = GyroToUnity(Input.gyro.attitude);
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+
+}
---
+++ UnityProject/Schnitzeljagt/Assets/TestScene/Gyro/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    public float Acceleration;
+    public float MaxMoveSpeed;
+    public float JumpForce;
+    
+    private Rigidbody2D rb;
+    private Transform camera;
+    private BoxCollider2D col;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        camera = Camera.main.transform;
+        col = GetComponent<BoxCollider2D>();
+	}
+	
+
+	void Update ()
+    {
+        HandleMovement();
+	}
+
+
+    void HandleMovement()
+    {
+        float xMove = Vector3.SignedAngle(Vector3.up, camera.rotation * Vector3.up, Vector3.forward) * -Acceleration;
+        float yMove = rb.velocity.y;
+
+        xMove = Mathf.Clamp(xMove, -MaxMoveSpeed, MaxMoveSpeed);
+        if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) 
+            || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Physics2D.OverlapCircle((transform.position + (Vector3)col.offset) + Vector3.down * (col.size.y/2f + 0.1f), 0.05f))
+                 yMove += JumpForce;
+        }
+
+        rb.velocity = new Vector2(xMove, yMove);
+
+    }
+}
