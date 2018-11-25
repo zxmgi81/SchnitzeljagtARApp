ffba6fccd106d618895257bf70d09f69b94637c6
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-11-25 18:30:19 +0100

    xml structure

--- UnityProject/Schnitzeljagt/Assets/XmlScene/Scripts/ArAppXmlHandler.cs
+++ UnityProject/Schnitzeljagt/Assets/XmlScene/Scripts/ArAppXmlHandler.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
+using System.IO;
 using UnityEngine;
-
 
 struct Character
 {
 
 }
-
 struct Chapter
 {
 
 }
 
+enum EventType
+{
+
+}
 
 public class ArAppXmlHandler : MonoBehaviour {
 
@@ -39,6 +42,13 @@ public class ArAppXmlHandler : MonoBehaviour {
 
     void LoadXML(int chapter)
     {
+        XmlReader xmlReader = null;// XmlReader.Create();
+
+        while(xmlReader.Read())
+        {
+
+        }
+
 
     }
 }
