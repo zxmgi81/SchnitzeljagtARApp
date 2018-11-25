c1680cb9bb364b687f73c7701592ea2c5043ee20
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-11-25 17:58:08 +0100

    XmlFileUpdated

---
+++ UnityProject/Schnitzeljagt/Assets/MinigameTest/BooteVersenken/Scripts/BoatGameMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatGameMaster : MonoBehaviour {
+
+    public static BoatGameMaster Instance;
+
+    private Vector3 BoatSpawnDiection;
+    private float BoatSpawnLength;
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+
+
+
+}
---
+++ UnityProject/Schnitzeljagt/Assets/XmlScene/Scripts/ArAppXmlHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+
+struct Character
+{
+
+}
+
+struct Chapter
+{
+
+}
+
+
+public class ArAppXmlHandler : MonoBehaviour {
+
+    private string path;
+
+	void Start ()
+    {
+        path = Application.dataPath + "/XmlFile/TestXml.xml";
+        
+
+
+
+
+    }
+	
+	
+	void Update ()
+    {
+		
+	}
+
+
+
+    void LoadXML(int chapter)
+    {
+
+    }
+}
