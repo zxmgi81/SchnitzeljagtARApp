7e58a8a9b795517237311acb0a550bd78ab4bfd3
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-11-25 15:31:19 +0100

    Flower Gyro Update

---
+++ UnityProject/Schnitzeljagt/Assets/MinigameTest/BooteVersenken/Scripts/Boot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boot : MonoBehaviour {
+
+
+    public float MoveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.right * Time.deltaTime * MoveSpeed;
+	}
+}
