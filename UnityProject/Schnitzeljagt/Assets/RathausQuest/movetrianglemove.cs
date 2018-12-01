a7ee1328801ba2b01ec25dcccaf98a985e8527b5
ansable <anchous12@gmail.com>
2018-12-01 14:20:06 +0200

    rathaus_quest
    new concept; yet w/o enemy script;
    (but it's in process)

---
+++ UnityProject/Schnitzeljagt/Assets/RathausQuest/movetrianglemove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movetrianglemove : MonoBehaviour {
+
+    public bool FaceRight;
+    public float speed = 50;
+    public float power = 12;
+
+    //Optional: in case if we want change speeds depending on difficulty
+    //otherwise to be initialized up
+
+    void Update()
+    {
+        MakeTheMove();
+    }
+
+    void MakeTheMove()
+    {
+        float moveOnX = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveOnX * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y, 0);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+        if (Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, power), ForceMode2D.Impulse);
+    }
+
+}
