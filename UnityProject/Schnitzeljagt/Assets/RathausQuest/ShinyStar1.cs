cc91c9168eca4a3cfd6de50d19fb980ea487eda1
ansable <anchous12@gmail.com>
2018-12-03 11:20:08 +0200

    star animations

---
+++ UnityProject/Schnitzeljagt/Assets/RathausQuest/ShinyStar1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinyStar1 : MonoBehaviour {
+
+    public GameObject star1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        healthyhealth.stars -= 1;
+        star1.gameObject.SetActive(false);
+    }
+}
